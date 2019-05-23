0b51dc5e07636d7e94375207790ae2a216fc7478
Pedro Henrique Alves Mourão Ceobaniuc <017833032356@rrows18242.tre-ro.jus.br>
2019-05-23 18:09:23 -0400

    continuação da aulda do onebitfood

---
+++ app/models/restaurant.rb
@@ -0,0 +1,16 @@
+class Restaurant < ApplicationRecord
+  belongs_to :category
+  has_many :product_categories
+  has_many :orders
+  has_many :reviwes
+
+  validates_assciated :category
+  valideres :name, presence: true
+  valideres :status, presence: true
+  valideres :delivery_tax, presence: true
+  valideres :city, presence: true
+  valideres :street, presence: true
+
+  enum status: { closed: 0, open: 1 }
+  has_one_attached :image
+end
